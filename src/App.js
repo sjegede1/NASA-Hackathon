52234b1ad160dd4e863ff5dcbd0053c8108be18c
Sola Jegede <sjegede1@gmail.com>
2023-08-24 09:37:52 -0500

    async

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ImageContainer from './components/ImageContainer';
 
 function App() {
   return (
     <div className="App">
-      
+      <ImageContainer />
     </div>
   );
 }
--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Carousel from './Carousel'
+import axios from 'axios'
+import Description from './Description'
 
 function ImageContainer() {
     const [data, setData] = useState([])
     const [currentData, setCurrentData] = useState({})
-    useEffect(async () => {
-        const res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=VMV9vIgFB7Mfpe28RjcFSDyawICxwznNYKIYYQCS&count=10`)
-        const data = await res.data
-        setCurrentData(data[0])
+    useEffect(() => {
+        const start = async () => {
+            const res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=VMV9vIgFB7Mfpe28RjcFSDyawICxwznNYKIYYQCS&count=10`)
+            const data = await res.data
+            setCurrentData(data[0])
+        }
+        start()
     })
     return (
         <div className="container">
