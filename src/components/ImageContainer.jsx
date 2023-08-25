bc36ac9275047333dae4855c80f2f4f4eff59be4
Sola Jegede <sjegede1@gmail.com>
2023-08-24 20:39:13 -0500

    carousel working

--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Carousel from './Carousel'
 import axios from 'axios'
 import Description from './Description'
+import "./ImageContainer.css"
 
 function ImageContainer() {
     const [data, setData] = useState([])
@@ -9,11 +10,11 @@ function ImageContainer() {
     useEffect(() => {
         const start = async () => {
             const res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=VMV9vIgFB7Mfpe28RjcFSDyawICxwznNYKIYYQCS&count=10`)
-            const data = await res.data
-            setCurrentData(data[0])
+            await setData(res.data)
+            setCurrentData(res.data[0])
         }
         start()
-    })
+    },[])
     return (
         <div className="container">
             <Carousel data={data} />
