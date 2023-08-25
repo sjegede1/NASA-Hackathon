91094f3f15b66d2cbc005de55b0b4d47c2cc49cc
Sola Jegede <sjegede1@gmail.com>
2023-08-24 21:56:10 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 import ImageContainer from './components/ImageContainer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ImageContainer />
     </div>
   );
